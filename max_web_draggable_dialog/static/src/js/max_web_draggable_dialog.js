a0e0ab3bac11569cba2fe3dc568e4eac060caf0b
Henry Zhou <zhouhenry@live.com>
2016-10-27 15:23:08 +0800

    Solve module web_draggable_dialog issue: to make export data dialog window draggable; Update module web_blue_theme to change dialog window exit button's color more lighter.

--- max_web_draggable_dialog/static/src/js/max_web_draggable_dialog.js
+++ max_web_draggable_dialog/static/src/js/max_web_draggable_dialog.js
@@ -7,6 +7,10 @@ odoo.define('max_web_draggable_dialog.max_web_draggable_dialog', function (requi
         open: function () {
             var self = this;
             this._super.apply(this, arguments);
+
+            // To make export data dialog draggable
+            self.$modal.modal('show');
+
             $(".modal.in").draggable({
                 handle: ".modal-header"
             });
