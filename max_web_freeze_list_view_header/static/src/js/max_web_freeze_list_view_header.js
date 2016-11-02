55a3aab9abd90e78f13c763d38b99e3369bf1889
Zhou Junli <zhouhenry@live.com>
2016-11-02 12:58:20 +0800

    aviod freeze header in many2many field.

--- max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header.js
+++ max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header.js
@@ -1,12 +1,12 @@
 ;(function ($, window, undefined){
     var one2many_length = $('.o_form_field.o_form_field_one2many').length;
+    var many2many_length = $('.o_form_field.o_form_field_many2many').length;
     var scrollArea = $(".o_content")[0];
-    if(scrollArea && one2many_length == 0) {
+    if(scrollArea && one2many_length == 0 && many2many_length == 0) {
         $('table.o_list_view').each(function () {
             $(this).stickyTableHeaders({scrollableArea: scrollArea});
         });
     }
-
     $(window).resize(function() {
         $('table.o_list_view').each(function () {
             $(this).stickyTableHeaders({scrollableArea: scrollArea});
