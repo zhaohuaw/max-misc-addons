f05e5d5522dc2a2f9c831c47e20ce4416e91bf04
Zhou Junli <zhouhenry@live.com>
2016-11-02 22:16:34 +0800

    fix-web-freeze-header-module-bug-in-group-view

--- max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header.js
+++ max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header.js
@@ -1,15 +1,23 @@
-;(function ($, window, undefined){
+var freeze_list_view_header_window_resize_event_is_bound = false;
+
+function freeze_list_view_header(){
     var one2many_length = $('.o_form_field.o_form_field_one2many').length;
     var many2many_length = $('.o_form_field.o_form_field_many2many').length;
     var scrollArea = $(".o_content")[0];
+
     if(scrollArea && one2many_length == 0 && many2many_length == 0) {
         $('table.o_list_view').each(function () {
             $(this).stickyTableHeaders({scrollableArea: scrollArea});
         });
-        $(window).resize(function() {
-            $('table.o_list_view').each(function () {
-                $(this).stickyTableHeaders({scrollableArea: scrollArea});
+        if(!freeze_list_view_header_window_resize_event_is_bound) {
+            $(window).resize(function () {
+                $('table.o_list_view').each(function () {
+                    $(this).stickyTableHeaders({scrollableArea: scrollArea});
+                });
             });
-        });
+            freeze_list_view_header_window_resize_event_is_bound = true;
+        }
     }
-})(jQuery,window);
+}
+
+freeze_list_view_header();
---
+++ max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header_grouping.js
@@ -0,0 +1,16 @@
+odoo.define('max_web_freeze_list_View_header.grouping', function (require) {
+'use strict';
+
+    var ListView = require('web.ListView');
+
+    ListView.Groups.include({
+        render_groups: function () {
+            var self = this;
+            var placeholder = this._super.apply(this, arguments);
+            var grouping_freezer = document.createElement("script");
+            grouping_freezer.innerText = "$('.o_group_header').click(function(){setTimeout('freeze_list_view_header();',200);})";
+            placeholder.appendChild(grouping_freezer);
+            return placeholder;
+        },
+    });
+});
