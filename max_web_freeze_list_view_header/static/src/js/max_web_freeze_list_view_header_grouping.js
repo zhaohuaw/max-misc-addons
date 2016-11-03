4708e824d39e9197390c699daf611cd2fb513d57
Zhou Junli <zhouhenry@live.com>
2016-11-03 14:43:06 +0800

    adjust async time out to 500ms for big record amount.

--- max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header_grouping.js
+++ max_web_freeze_list_view_header/static/src/js/max_web_freeze_list_view_header_grouping.js
@@ -8,7 +8,7 @@ odoo.define('max_web_freeze_list_View_header.grouping', function (require) {
             var self = this;
             var placeholder = this._super.apply(this, arguments);
             var grouping_freezer = document.createElement("script");
-            grouping_freezer.innerText = "$('.o_group_header').click(function(){setTimeout('freeze_list_view_header();',200);})";
+            grouping_freezer.innerText = "$('.o_group_header').click(function(){setTimeout('freeze_list_view_header();',500);})";
             placeholder.appendChild(grouping_freezer);
             return placeholder;
         },
