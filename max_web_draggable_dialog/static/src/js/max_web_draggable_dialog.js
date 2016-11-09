04437c48f14b16026944d12b55f50a3874958fa4
Zhou Junli <zhouhenry@live.com>
2016-11-09 21:11:14 +0800

    fix draggable js code by using .done()

--- max_web_draggable_dialog/static/src/js/max_web_draggable_dialog.js
+++ max_web_draggable_dialog/static/src/js/max_web_draggable_dialog.js
@@ -1,18 +1,15 @@
-odoo.define('max_web_draggable_dialog.max_web_draggable_dialog', function (require) {
+odoo.define('max_web_draggable_dialog', function (require) {
 'use strict';
 
     var Dialog = require('web.Dialog');
 
     Dialog.include({
         open: function () {
-            var self = this;
             this._super.apply(this, arguments);
-
-            // To make export data dialog draggable
-            self.$modal.modal('show');
-
-            $(".modal.in").draggable({
-                handle: ".modal-header"
+            this._opened.done(function(){
+                $(".modal.in").draggable({
+                    handle: ".modal-header"
+                });
             });
             return this;
         },
